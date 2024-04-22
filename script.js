9126c3da723d6fa42b6ebb494c2a25f9c9da0a4f
Vaishnavi Rajput <162553453+rajput20012003@users.noreply.github.com>
2024-04-22 21:27:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your code here
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  var draggedElement = document.getElementById(data);
+  var droppedElement = ev.target;
+
+  if (draggedElement !== droppedElement) {
+    var temp = draggedElement.innerHTML;
+    draggedElement.innerHTML = droppedElement.innerHTML;
+    droppedElement.innerHTML = temp;
+  }
+}
+
+var images = document.querySelectorAll('.image');
+
+images.forEach(image => {
+  image.addEventListener('dragover', allowDrop);
+  image.addEventListener('drop', drop);
+});
+
